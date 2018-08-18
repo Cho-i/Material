c3bf374bccdf725298faa33bfc828aaf5531a696
Cho-i <cho.i.lee.1223@gmail.com>
2018-08-18 16:43:23 +0900

    material

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>Material UI</h1>
     <ul>
-      <li><router-link to="/content">Content</router-link></li>
+      <li><router-link to="/">Content</router-link></li>
       <li><router-link to="/sample">Sample</router-link></li>
     </ul>
     <div class="view">
@@ -20,7 +20,7 @@
     mode:'history',
     routes:[
       {path:'/sample',component:Sample},
-      {path:'/content',component:Content}
+      {path:'/',component:Content}
     ]
   });
   export default {
@@ -34,6 +34,6 @@
   ul{list-style-type:none;margin:0;padding:0;overflow:hidden;background:#fff;}
   ul li{float:left;width:50%;}
   ul li a{display:block;padding:10px 0;text-align:center;font-size:18px;color:#000 !important;}
-  ul li a:hover,.router-link-active{background:#000;color:#fff !important;text-decoration:none !important;}
+  ul li a:hover,.router-link-exact-active{background:#000;color:#fff !important;text-decoration:none !important;}
   .view{padding:30px 0;}
 </style>
--- src/Content.vue
+++ src/Content.vue
@@ -1,13 +1,32 @@
 <template>
-	<p>test</p>
+	<div id="content">
+		<template>
+			<h2>Material Design</h2>
+			<a v-bind:href="link1" target="_blank">Material Design 소개</a>
+			<a v-bind:href="link2" target="_blank">Material Design 살펴보기</a>
+			<br>
+			<h2>Material UI</h2>
+			<a v-bind:href="link3" target="_blank">https://material.io</a>
+			<a v-bind:href="link4" target="_blank">https://vuematerial.io/</a>
+		</template>
+
+	</div>
 </template>
+
 
 <script>
 	export default {
-		name: "Content"
+		name: 'content',
+		data: () => ({
+			link1:'https://developers-kr.googleblog.com/2014/07/this-is-material-design.html',
+			link2:'https://www.slideshare.net/watchncompass/3-material-design',
+			link3:'https://material.io',
+			link4:'https://vuematerial.io/',
+		})
 	}
 </script>
 
-<style scoped>
-
+<style>
+a{display:block;padding:5px;color:#000 !important;}
+a:hover{}
 </style>
